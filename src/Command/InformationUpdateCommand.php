7187134d93bda786494b7c401e182d476b2dd4ef
Julien Arbey <jarbey@talentsoft.com>
2017-12-24 13:51:33 +0100

    Init SF4

---
+++ src/Command/InformationUpdateCommand.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jarbey
+ * Date: 24/12/2017
+ * Time: 13:33
+ */
+
+namespace App\Command;
+
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class InformationUpdateCommand extends Command {
+
+	protected function configure()
+	{
+		$this
+			->setName('information:update')
+			->setDescription('Send information to databse')
+			->setHelp('This command get current values from sensor and push-it into the database')
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$output->writeln('test');
+	}
+
+
+}
