073e0ed65ec4af6b2e81c14af8910ef97cd3a118
Julien Arbey <jarbey@talentsoft.com>
2018-01-14 19:26:34 +0100

    Add multiple sensors into update command

--- screen.sh
+++ screen.sh
@@ -6,3 +6,5 @@ do
         ip=`ifconfig | grep inet | grep -v inet6 | grep -v "127.0.0.1" | awk '{ print $2 }'`
 done
 /home/pi/cellar/exec_display.py "$ip" &
+
+/usr/bin/php /home/pi/cellar/bin/console bin/console cellar:information:update
