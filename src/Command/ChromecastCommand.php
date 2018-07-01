d61caf800c8d60609b71d89bd7b26db5be21e3cf
Julien ARBEY <julien@arbey.fr>
2018-07-01 18:42:03 +0200

    * Add custom receiver

--- src/Command/ChromecastCommand.php
+++ src/Command/ChromecastCommand.php
@@ -32,6 +32,6 @@ class ChromecastCommand extends AbstractCommand {
 
     protected function execute(InputInterface $input, OutputInterface $output) {
         $output->writeln('<info>Start Chromecast</info>');
-        $this->chromecastManager->DMP->play('http://cellar.arbey.fr/1');
+        $this->chromecastManager->DMP->play('https://cellar.arbey.fr/1');
     }
 }
