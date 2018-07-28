c09a51cf5e8fd643d86dc6342b326984eac8de3b
Julien ARBEY <julien@arbey.fr>
2018-07-28 20:24:15 +0200

    * WinePairing

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,6 +6,7 @@ use App\Entity\Bottle;
 use App\Entity\Db;
 use App\Repository\DbRepository;
 use App\Service\CavusviniferaManager;
+use App\Service\PlatsnetvinsManager;
 use App\Service\WineManager;
 use Doctrine\ORM\EntityNotFoundException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -38,19 +39,6 @@ class IndexController extends Controller {
 			throw new EntityNotFoundException();
 		}
 
-		if ($request->query->get('from', 0) > 0) {
-			$from = new \DateTime('@' . $request->query->get('from', 0), new \DateTimeZone('UTC'));
-		} else {
-			$from = new \DateTime();
-			$from->sub(new \DateInterval('P1D'));
-		}
-
-		if ($request->query->get('to', 0) > 0) {
-			$to = new \DateTime('@' . $request->query->get('to', 0), new \DateTimeZone('UTC'));
-		} else {
-			$to = new \DateTime();
-		}
-
 		$stats_color = [];
 		foreach ($wineManager->getWineColorRepository()->findAll() as $wine_color) {
 			$stats_color[$wine_color->getName()] = $wineManager->getWineBottleRepository()->getNbWineBottleColor($wine_color);
@@ -58,12 +46,116 @@ class IndexController extends Controller {
 
 		return $this->render('home.html.twig', [
 			'db' => $db,
-			'from' => $from,
-			'to' => $to,
 			'ws_url' => 'cellar.arbey.fr/ws',
 			'stats_color' => $stats_color,
 		]);
 	}
+
+    /**
+     * @Route("/{id}/temperature", name="temperature", requirements={"id" = "\d+"})
+     *
+     * @param $id
+     * @param Request $request
+     * @param DbRepository $db_repository
+     * @param WineManager $wineManager
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws EntityNotFoundException
+     * @throws \Exception
+     */
+    public function temperature($id, Request $request, DbRepository $db_repository, WineManager $wineManager) {
+        /** @var Db $db */
+        $db = $db_repository->find($id);
+        if ($db == null) {
+            throw new EntityNotFoundException();
+        }
+
+        if ($request->query->get('from', 0) > 0) {
+            $from = new \DateTime('@' . $request->query->get('from', 0), new \DateTimeZone('UTC'));
+        } else {
+            $from = new \DateTime();
+            $from->sub(new \DateInterval('P1D'));
+        }
+
+        if ($request->query->get('to', 0) > 0) {
+            $to = new \DateTime('@' . $request->query->get('to', 0), new \DateTimeZone('UTC'));
+        } else {
+            $to = new \DateTime();
+        }
+
+        return $this->render('home.html.twig', [
+            'db' => $db,
+            'from' => $from,
+            'to' => $to,
+            'ws_url' => 'cellar.arbey.fr/ws',
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/pairing", name="pairing", requirements={"id" = "\d+"})
+     *
+     * @param $id
+     * @param Request $request
+     * @param DbRepository $db_repository
+     * @param PlatsnetvinsManager $platsnetvins_manager
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws EntityNotFoundException
+     * @throws \Exception
+     */
+    public function pairing($id, Request $request, DbRepository $db_repository, PlatsnetvinsManager $platsnetvins_manager) {
+        /** @var Db $db */
+        $db = $db_repository->find($id);
+        if ($db == null) {
+            throw new EntityNotFoundException();
+        }
+
+        $search = $request->query->get('search');
+        if (!$search) {
+            throw new EntityNotFoundException();
+        }
+
+        $result = $platsnetvins_manager->getWineBottlesPair($search);
+        $raw_result = $result->getFood() . "\n";
+        $raw_result .= 'BEST' . "\n";
+        foreach ($result->getWinesBest() as $wine_best) {
+            $raw_result .= '-> ' . $wine_best->getWineColor()->getName() . ' : ' .
+                $wine_best->getWineArea()->getAreaName() . ' - ' .
+                $wine_best->getWineArea()->getRegion() . ' => ' .
+                count($wine_best->getWineArea()->getBottles()) . "\n";
+
+            foreach ($wine_best->getWineArea()->getBottles() as $bottle) {
+                if ($bottle->getStocks()) {
+                    foreach ($bottle->getStocks() as $stock) {
+                        if ($stock->getQuantityCurrent() > 0) {
+                            $raw_result .= '  => ' . $stock->getQuantityCurrent() . 'x ' . $bottle->getVintage() . ' - ' . $bottle->getName() . ' : (' . $bottle->getBottleSize()->getCapacity() . ' cl)' . "\n";
+                        }
+                    }
+                }
+            }
+
+        }
+        $raw_result .= 'GOOD' . "\n";
+        foreach ($result->getWinesGood() as $wine_best) {
+            $raw_result .= '-> ' . $wine_best->getWineColor()->getName() . ' : ' .
+                $wine_best->getWineArea()->getAreaName() . ' - ' .
+                $wine_best->getWineArea()->getRegion() . ' => ' .
+                count($wine_best->getWineArea()->getBottles()) . "\n";
+
+            foreach ($wine_best->getWineArea()->getBottles() as $bottle) {
+                if ($bottle->getStocks()) {
+                    foreach ($bottle->getStocks() as $stock) {
+                        if ($stock->getQuantityCurrent() > 0) {
+                            $raw_result .= '  => ' . $stock->getQuantityCurrent() . 'x ' . $bottle->getVintage() . ' - ' . $bottle->getName() . ' : (' . $bottle->getBottleSize()->getCapacity() . ' cl)' . "\n";
+                        }
+                    }
+                }
+            }
+        }
+
+        return $this->render('pairing.html.twig', [
+            'db' => $db,
+            'pairing' => $raw_result,
+        ]);
+    }
 
 	/**
 	 * @Route("/{id}/list", name="cellar_list", requirements={"id" = "\d+"})
