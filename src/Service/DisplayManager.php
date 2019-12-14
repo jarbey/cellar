989f540531d4d68a607d992c92c5899ba43356b5
Julien ARBEY <julien@arbey.fr>
2019-12-14 19:58:41 +0100

    * Update

--- src/Service/DisplayManager.php
+++ src/Service/DisplayManager.php
@@ -92,29 +92,31 @@ class DisplayManager extends AbstractManager {
 	 * @param SensorDataGroup $sensor_data
 	 */
 	public function displaySensorData(SensorDataGroup $sensor_data) {
-		// Date top-right
-		$display_data = [new Display(date('d/m/Y H:i:s'), new DisplayFont(self::FONT_SIZE_DATE), new DisplayPosition(300, 0), DisplayColor::white())];
+        if ($this->screen_enable) {
+            // Date top-right
+            $display_data = [new Display(date('d/m/Y H:i:s'), new DisplayFont(self::FONT_SIZE_DATE), new DisplayPosition(300, 0), DisplayColor::white())];
 
-		// Data for each SensorData
-		$y_offset = (count($sensor_data->getSensorData()) - 1) * self::OFFSET_DATA * -1;
-		/** @var SensorData $data */
-		foreach ($sensor_data->getSensorData() as $data) {
-			$this->getLogger()->debug('Gpio {gpio} : T {temperature} ; H {humidity} ; Tlim {t_limit} ; Hlim {h_limit}', [
-				'gpio' => $data->getSensor()->getGpio(),
-				'temperature' => $data->getTemperature(),
-				'humidity' => $data->getHumidity(),
-				't_limit' => $data->getSensor()->getTemperatureLimit()->__toString(),
-				'h_limit' => $data->getSensor()->getHumidityLimit()->__toString(),
-			]);
+            // Data for each SensorData
+            $y_offset = (count($sensor_data->getSensorData()) - 1) * self::OFFSET_DATA * -1;
+            /** @var SensorData $data */
+            foreach ($sensor_data->getSensorData() as $data) {
+                $this->getLogger()->debug('Gpio {gpio} : T {temperature} ; H {humidity} ; Tlim {t_limit} ; Hlim {h_limit}', [
+                    'gpio' => $data->getSensor()->getGpio(),
+                    'temperature' => $data->getTemperature(),
+                    'humidity' => $data->getHumidity(),
+                    't_limit' => $data->getSensor()->getTemperatureLimit()->__toString(),
+                    'h_limit' => $data->getSensor()->getHumidityLimit()->__toString(),
+                ]);
 
-			$display_data[] = new Display($data->getTemperature() . "°C", new DisplayFont(self::FONT_SIZE_DATA), new DisplayPosition(30, 130 + $y_offset), $data->getSensor()->getTemperatureLimit()->getColor($data->getTemperature()));
-			$display_data[] = new Display($data->getHumidity() . " %", new DisplayFont(self::FONT_SIZE_DATA), new DisplayPosition(260, 130 + $y_offset), $data->getSensor()->getHumidityLimit()->getColor($data->getHumidity()));
+                $display_data[] = new Display($data->getTemperature() . "°C", new DisplayFont(self::FONT_SIZE_DATA), new DisplayPosition(30, 130 + $y_offset), $data->getSensor()->getTemperatureLimit()->getColor($data->getTemperature()));
+                $display_data[] = new Display($data->getHumidity() . " %", new DisplayFont(self::FONT_SIZE_DATA), new DisplayPosition(260, 130 + $y_offset), $data->getSensor()->getHumidityLimit()->getColor($data->getHumidity()));
 
-			$y_offset += self::FONT_SIZE_DATA + self::FONT_MARGIN_DATA;
-		}
+                $y_offset += self::FONT_SIZE_DATA + self::FONT_MARGIN_DATA;
+            }
 
-		// Send to display
-		$this->sendDisplay($display_data);
+            // Send to display
+            $this->sendDisplay($display_data);
+        }
 	}
 
 	function __destruct() {
